133502755f730bee21ab1ac57aefc3dc34641895
jdelg140 <jacobdelgado140@gmail.com>
2023-06-05 17:40:28 -0400

    Formatted all the js files to follow best practices

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,30 +1,30 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-if(license!=="none"){
-  return `![License Badge](https://img.shields.io/badge/License-${license}-orange)`
-}
-return ""
+  if (license !== "none") {
+    return `![License Badge](https://img.shields.io/badge/License-${license}-orange)`
+  }
+  return ""
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if(license!=="none"){
+  if (license !== "none") {
     return `* [License](#license)`
   }
   return ""
- }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if(license!=="none"){
+  if (license !== "none") {
     return `## License
 This project is licensed under the ${license} license.`
   }
   return ""
- }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
--- utils/index.js
+++ utils/index.js
@@ -1,29 +1,29 @@
 // TODO: Include packages needed for this application
-const path=require("path")
-const fs= require("fs")
-const inquirer=require("inquirer")
+const path = require("path")
+const fs = require("fs")
+const inquirer = require("inquirer")
 const generateMarkdown = require("./generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type:"input",
-        name:"title",
+        type: "input",
+        name: "title",
         message: "Name of Project."
     },
     {
-        type:"input",
+        type: "input",
         name: "description",
-        message:"Describe the functionality and purpose of your project"
+        message: "Describe the functionality and purpose of your project"
     },
     {
         type: "list",
         name: "license",
         message: "Select the license/licenses used for this project",
-        choices: ["MIT","APACHE 2.0", "GPL 3.0","BSD 3","none"]
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "none"]
     },
     {
-        type:"input",
+        type: "input",
         name: "require",
         message: "List project dependencies here."
     },
@@ -33,58 +33,58 @@ const questions = [
         message: "State the coding languages used in this project."
     },
     {
-        type:"input",
+        type: "input",
         name: "creator",
-        message:"Provide your GitHub username."
+        message: "Provide your GitHub username."
     },
     {
-        type:"input",
-        name:"name",
+        type: "input",
+        name: "name",
         message: "Enter your full name."
     },
     {
-        type:"input",
-        name:"email",
+        type: "input",
+        name: "email",
         message: "Enter your email address."
     },
     {
-        type:"input",
-        name:"contributors",
+        type: "input",
+        name: "contributors",
         message: "Enter any contributers and their GitHub usernames."
     },
     {
-        type:"input",
-        name:"test",
+        type: "input",
+        name: "test",
         message: "Provide a walkthrough of what tests are required if necessary."
     },
     {
-        type:"input",
-        name:"install",
+        type: "input",
+        name: "install",
         message: "Provide a resource to install software necessary to run the app."
     },
     {
-        type:"input",
-        name:"questions",
+        type: "input",
+        name: "questions",
         message: "Provide any questions regarding this app."
     },
     {
-        type:"input",
-        name:"link",
+        type: "input",
+        name: "link",
         message: "Provide a link to your github page."
     }
 
 
 ];
 
- // TODO: Create a function to initialize app
-function writeToFile(fileName,data) {
-    return fs.writeFileSync(path.join(process.cwd(),fileName),data)
+// TODO: Create a function to initialize app
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
 }
 
 // TODO: Create a function to write README file
 function init() {
-    inquirer.prompt(questions).then((responses)=>{
-        writeToFile("README.md",generateMarkdown({...responses}))
+    inquirer.prompt(questions).then((responses) => {
+        writeToFile("README.md", generateMarkdown({ ...responses }))
     })
 }
 
